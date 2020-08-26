69c9d53c3fa31edb900b77aae168904521b43efd
Gonzalo Guerrero <contacto@gonzaloelias.me>
2020-08-26 00:46:49 -0400

    start with webpack

---
+++ src/app.js
@@ -0,0 +1,8 @@
+/* Otra forma de ejecutar el archiivo .scss es a traves de :
+    require ('./styles/main.scss'); */
+import "./styles/main.scss";
+import "./static/img/logo.png";
+
+/* document.body.innerHTML = '<h1>Nuevo texo con javascript</h1>'; */
+
+console.log('Bienvenidos');
